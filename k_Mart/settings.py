3065ce06af869140c4db3c6fd6b737df6a3ab853
Mayur Tembhare <135987777+MayurTembhare20@users.noreply.github.com>
2023-06-20 19:58:00 +0530

    First Commit
    This Is My First Commit

--- k_Mart/settings.py
+++ k_Mart/settings.py
@@ -91,14 +91,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
-            'ENGINE': 'django.db.backends.mysql',
-                'NAME': 'mayur',
-                'USER': 'root',
-                'HOST': 'localhost',
-                'PORT': '3306',
-                'PASSWORD': 'Maysql@202',
-        }
+    
 }
 
 
